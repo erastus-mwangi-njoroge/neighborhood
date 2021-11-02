81084edc038923da58b6f69d74187974a3a1bc9f
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-11-02 22:07:34 +0300

    add adjustments to majirani urls

--- majirani/urls.py
+++ majirani/urls.py
@@ -1,25 +1,24 @@
-from django.urls import path
-from django.urls import include
+from django.urls import re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    path(r'^$',views.index,name='Index'),
-    path(r'^notifications',views.notification, name='notifications'),
-    path(r'^blog',views.blog, name='blog'),
-    path(r'^health',views.health, name='health'),
-    path(r'^authorities',views.authorities, name='authorities'),
-    path(r'^businesses',views.businesses, name='businesses'),
-    path(r'^view/blog/(\d+)',views.view_blog,name='view_blog'),
-    path(r'^my-profile/',views.my_profile, name='my-profile'),
-    path(r'^user/(?P<username>\w{0,50})',views.user_profile,name='user-profile'),
-    path(r'^new/blogpost$',views.new_blogpost, name='new-blogpost'),
-    path(r'^new/business$',views.new_business, name='new-business'),
-    path(r'^create/profile$',views.create_profile, name='create-profile'),
-    path(r'^new/notification$',views.new_notification, name='new-notification'),
-    path(r'^update/profile$',views.update_profile, name='update-profile'),
-    path(r'^search/',views.search_results, name='search_results'),
+    re_path(r'^$',views.index,name='Index'),
+    re_path(r'^notifications',views.notification, name='notifications'),
+    re_path(r'^blog',views.blog, name='blog'),
+    re_path(r'^health',views.health, name='health'),
+    re_path(r'^authorities',views.authorities, name='authorities'),
+    re_path(r'^businesses',views.businesses, name='businesses'),
+    re_path(r'^view/blog/(\d+)',views.view_blog,name='view_blog'),
+    re_path(r'^my-profile/',views.my_profile, name='my-profile'),
+    re_path(r'^user/(?P<username>\w{0,50})',views.user_profile,name='user-profile'),
+    re_path(r'^new/blogpost$',views.new_blogpost, name='new-blogpost'),
+    re_path(r'^new/business$',views.new_business, name='new-business'),
+    re_path(r'^create/profile$',views.create_profile, name='create-profile'),
+    re_path(r'^new/notification$',views.new_notification, name='new-notification'),
+    re_path(r'^update/profile$',views.update_profile, name='update-profile'),
+    re_path(r'^search/',views.search_results, name='search_results'),
 
 ]
 
