e78d629fe7f65072d6829f3cb5ef20daec6e5f32
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-11-02 21:52:56 +0300

    add to admin.py

--- majirani/admin.py
+++ majirani/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import neighbourhood,healthservices,notifications,Business,Health,PoliceAuthorities,Blog,Profile
+
+
+class HealthAdmin(admin.ModelAdmin):
+    filter_horizontal =['healthservices']
 
 # Register your models here.
+admin.site.register(neighbourhood)
+admin.site.register(Health,HealthAdmin)
+admin.site.register(Business)
+admin.site.register(healthservices)
+admin.site.register(PoliceAuthorities)
+admin.site.register(Blog)
+admin.site.register(Profile)
+admin.site.register(notifications)
